5c1029110e5c75b33e673693c1b8c6932dc2c6c6
sohil <sohils413@gmail.com>
2025-10-27 23:59:28 +0530

    make assessment migration and model and install spatie medialibrary package

---
+++ database/migrations/2025_10_27_174808_create_assessments_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assessments', function (Blueprint $table) {
+            $table->id()->comment('Primary key: Unique assessment ID');
+
+            $table->foreignId('assessment_id')->nullable()->constrained()->cascadeOnDelete()->cascadeOnUpdate()->comment('Parent assessment ID if applicable');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate()->comment('The ID of the user whose assessment is to be created');
+            $table->foreignId('clinic_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate()->comment('Clinic associated with this assessment');
+            $table->foreignId('created_by')->nullable()->constrained('users')->cascadeOnDelete()->cascadeOnUpdate()->comment('Who created the assessment');
+            
+            $table->integer('age')->nullable()->comment('Patient age in years');
+            $table->string('daily_sun_exposure_hours')->nullable()->comment('Average daily sun exposure in hours');
+            $table->enum('social_event', ['yes', 'no'])->default('no')->comment('Whether the patient has recent or upcoming social events');
+            $table->enum('upcoming_travel', ['yes', 'no'])->default('no')->comment('Whether the patient has upcoming travel plans');
+            $table->json('medical_history')->nullable()->comment('Patient medical history in JSON format');
+            $table->json('allergies')->nullable()->comment('List of patient allergies in JSON format');
+            $table->boolean('is_pregnant')->default(false)->comment('Whether the user is pregnant (true/false)');
+            $table->enum('breastfeeding', ['yes', 'no'])->nullable()->comment('Whether the patient is currently breastfeeding');
+            $table->json('diagnosis')->nullable()->comment('Diagnosis details in JSON format');
+            $table->json('parameters_with_abnormal_scores')->nullable()->comment('Parameters with abnormal scores (JSON array or object)');
+            $table->string('treatment_plan_type')->nullable()->comment('Type of treatment plan (e.g., basic, advanced, customized)');
+            $table->json('treatment_plan')->nullable()->comment('Treatment plan details in JSON format');
+
+            $table->softDeletes();
+            $table->timestamps();
+
+            // Indexes
+            $table->index('user_id', 'idx_assessments_user_id');
+            $table->index('treatment_plan_type', 'idx_assessments_treatment_plan_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assessments');
+    }
+};
