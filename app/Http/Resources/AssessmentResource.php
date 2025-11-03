cc3588bc1f6b9098a8ba5cc3700a2cbfaaf0d210
svsatish820 <svsatish820@gmail.com>
2025-11-03 20:25:30 +0530

    status not updating bug fixed

--- app/Http/Resources/AssessmentResource.php
+++ app/Http/Resources/AssessmentResource.php
@@ -35,6 +35,8 @@ class AssessmentResource extends BaseResource
             'parameters_with_abnormal_scores' => $this->parameters_with_abnormal_scores,
             'treatment_plan_type' => $this->treatment_plan_type,
             'treatment_plan' => $this->treatment_plan,
+            'status' => $this->status,
+            'therapist_notes' => $this->therapist_notes,
             'images' => $this->getMedia('assessment_images')->map(function (Media $media) {
                 return [
                     'id' => $media->id,
