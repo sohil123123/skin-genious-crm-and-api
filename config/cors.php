707c3465e8245c656f2e87eb29f6064cf4e716ad
svsatish820 <svsatish820@gmail.com>
2025-11-10 13:04:44 +0530

    image url cors origin issue resolved

---
+++ config/cors.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'storage/*'],
+    'allowed_methods'   => ['*'],
+    'allowed_origins'   => ['http://localhost:9000', 'https://aiaesthetics.cbphysiotherapy.in'],
+    'allowed_origins_patterns' => [
+        '/^https?:\/\/([a-z0-9-]+\.)?annaponsprojects\.com$/',
+    ],
+    'allowed_headers'   => ['*'],
+    'exposed_headers'   => [],
+    'max_age'           => 3600,
+    'supports_credentials' => false,
+];
+
